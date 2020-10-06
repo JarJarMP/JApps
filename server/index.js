847c570ef1ba1f57c83a0db5f0e9776c719d41a7
PeterMaroti <noemail@dummy.com>
2020-10-06 07:47:45 +0200

    add - express base to server

--- server/index.js
+++ server/index.js
@@ -1,1 +1,11 @@
-console.log('server package works');
+const express = require('express');
+const app = express();
+const port = 3000;
+
+app.get('/', (req, res) => {
+  res.send('Hello World!');
+})
+
+app.listen(port, () => {
+  console.log(`Example app listening at http://localhost:${port}`)
+});
